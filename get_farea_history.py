5b59fcc45c9502ec8cc0c2c7d90beeff46a9a334
zbecker <zebfullerbecker@gmail.com>
2025-01-12 11:20:34 -0800

    Disambiguate by regions; empty response check

--- get_farea_history.py
+++ get_farea_history.py
@@ -1,9 +1,9 @@
 """
-Script to query FEDS API for a specific fireID. 
+Script to query FEDS API for a specific fireID within a named region. 
 
 Outputs a CSV with a sorted timeseries of farea, primarykey, and perimeter centroids.
 
-E.g. -> farea_18007.csv
+E.g. -> farea_CONUS_415.csv
 """
 
 import requests
@@ -11,8 +11,9 @@ import pandas as pd
 import geopandas as gpd 
 from owslib.ogcapi.features import Features 
 
-FIRE_ID = 18007
-OUT_FILE_PREFIX = "farea_"
+FIRE_ID = 415
+REGION = "CONUS"
+OUT_FILE_PREFIX = "farea"
 
 OGC_URL = "https://firenrt.delta-backend.com"
 api = Features(url=OGC_URL)
@@ -20,14 +21,23 @@ api = Features(url=OGC_URL)
 res = api.collection_items(
     "public.eis_fire_lf_perimeter_nrt", 
     limit = 8000, 
-    filter = "fireid=" + str(FIRE_ID)
+    filter=f"fireid={str(FIRE_ID)} AND region='{REGION}'"
 )
 
+
+if len(res["features"]) < 1:
+    print(f"No fires found matching {REGION} fireid {FIRE_ID}")
+    exit()
+
 gdf = gpd.GeoDataFrame.from_features(res["features"]).set_crs("epsg:4326")
+
+print(f"Returned {len(gdf)} perimeters for {REGION} fireid {FIRE_ID}.")
 
 gdf["perim_t"] = pd.to_datetime(gdf["primarykey"].str.split('|').str[-1])
 
 gdf = gdf.sort_values(by="perim_t", ascending=True)
 gdf["centroid"] = gdf.geometry.centroid
 
-gdf[["primarykey", "farea", "centroid"]].to_csv(OUT_FILE_PREFIX + str(FIRE_ID) + ".csv", index=False) 
+outpath = "_".join([OUT_FILE_PREFIX, REGION, str(FIRE_ID)]) + ".csv"
+
+gdf[["primarykey", "farea", "centroid"]].to_csv(outpath, index=False) 
--- get_perimeters.py
+++ get_perimeters.py
@@ -1,5 +1,5 @@
 """
-Script to query FEDS API for a specific fireID. 
+Script to query FEDS API for a specific fireID within a named region. 
 
 Outputs a shapefile with each perimeter for that fire. 
 """
@@ -8,9 +8,9 @@ import pandas as pd
 import geopandas as gpd 
 from owslib.ogcapi.features import Features 
 
-# note: possible that different regions reuse fire IDs
-FIRE_ID = 18007
-OUT_FILE_PREFIX = "perimeters_"
+FIRE_ID = 415
+REGION = "CONUS"
+OUT_FILE_PREFIX = "perimeters"
 
 OGC_URL = "https://firenrt.delta-backend.com"
 api = Features(url=OGC_URL)
@@ -18,14 +18,19 @@ api = Features(url=OGC_URL)
 res = api.collection_items(
     "public.eis_fire_lf_perimeter_nrt", 
     limit = 8000, 
-    filter = "fireid=" + str(FIRE_ID)
+    filter=f"fireid={str(FIRE_ID)} AND region='{REGION}'"
 )
 
+if len(res["features"]) < 1:
+    print(f"No fires found matching {REGION} fireid {FIRE_ID}")
+    exit()
+
 gdf = gpd.GeoDataFrame.from_features(res["features"]).set_crs("epsg:4326")
+print(f"Returned {len(gdf)} perimeters for {REGION} fireid {FIRE_ID}.")
 
 gdf["perim_t"] = pd.to_datetime(gdf["primarykey"].str.split('|').str[-1])
 
 gdf = gdf.sort_values(by="perim_t", ascending=True)
 gdf.perim_t = gdf.perim_t.astype(str)
 
-gdf.to_file(OUT_FILE_PREFIX + str(FIRE_ID), index=False) 
+gdf.to_file("_".join([OUT_FILE_PREFIX, REGION, str(FIRE_ID)]), index=False) 
